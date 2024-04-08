de39c16b078b7ec4b5884b3d7b8148d075b4bc9e
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-09 01:57:48 +0200

    Add clinte-side router param guard

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -19,36 +19,44 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Home />,
       },
       {
-        path: "categories",
-        element: <Categories />
+        path: 'categories',
+        element: <Categories />,
       },
       {
-        path: "products/:prefix",
-        element: <Products />
+        path: 'products/:prefix',
+        element: <Products />,
+        loader: ({ params }) => {
+          if (typeof  params.prefix !== 'string' ||
+            !/^[a-z]+$/.test(params.prefix)) {
+            throw new Response('Bad Request', {
+              statusText: 'Category not found',
+              status: 400,
+            });
+          }
+          return true;
+        },
       },
       {
-        path: "about-us",
-        element: <AboutUs />
+        path: 'about-us',
+        element: <AboutUs />,
       },
       {
-        path: "login",
-        element: <Login />
+        path: 'login',
+        element: <Login />,
       },
       {
-        path: "regester",
-        element: <Regester />
-      }
-    ]
-  }
-])
+        path: 'regester',
+        element: <Regester />,
+      },
+    ],
+  },
+]);
 
 const AppRouter = () => {
-  return (
-    <RouterProvider  router={router}/>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default AppRouter
+export default AppRouter;
