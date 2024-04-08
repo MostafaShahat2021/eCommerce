a992780379de12ac5775bef7f7738ca839b1f8a6
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-08 10:51:23 +0200

    Create basic routes

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -3,13 +3,16 @@ import {Header, Footer} from "@components/common"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './styles.module.css'
+import { Outlet } from "react-router-dom";
 
 const {container, wrapper} = styles
 const MainLayout = () => {
   return (
     <Container className={container}>
         <Header />
-      <div className={wrapper}></div>
+      <div className={wrapper}>
+        <Outlet />
+      </div>
       <Footer />
     </Container>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,40 @@
 import ReactDOM from 'react-dom/client';
-import {MainLayout} from '@layouts/index';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//layouts
+import MainLayout from '@layouts/MainLayout/MainLayout';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<MainLayout />);
+//pages
+import Home from '@pages/Home';
+import Categories from '@pages/Categories';
+import Products from '@pages/Products';
+import AboutUs from '@pages/AboutUs';
+
+// styles
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "categories",
+        element: <Categories />
+      },
+      {
+        path: "products/:prefix",
+        element: <Products />
+      },
+      {
+        path: "about-us",
+        element: <AboutUs />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
