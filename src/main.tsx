bc09ddb248f7fff9c157f2f20b5bd6bf1422fa43
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-08 11:06:07 +0200

    react router Code Splitting

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-//layouts
-import MainLayout from '@layouts/MainLayout/MainLayout';
-
-//pages
-import Home from '@pages/Home';
-import Categories from '@pages/Categories';
-import Products from '@pages/Products';
-import AboutUs from '@pages/AboutUs';
-
+import AppRouter from '@routes/AppRouter';
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "categories",
-        element: <Categories />
-      },
-      {
-        path: "products/:prefix",
-        element: <Products />
-      },
-      {
-        path: "about-us",
-        element: <AboutUs />
-      }
-    ]
-  }
-])
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppRouter />);
