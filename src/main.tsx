85a6ca315a4cc13896b10be940712b7a1baa5bfd
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-10 00:59:19 +0200

    Create App Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import AppRouter from '@routes/AppRouter';
+// Redux
+import store from "@store/index"
+import { Provider } from 'react-redux';
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<AppRouter />);
+ReactDOM.createRoot(document.getElementById('root')!).render(<Provider store={store}><AppRouter /></Provider>);
