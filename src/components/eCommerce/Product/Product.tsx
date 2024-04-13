54cba96ce4260badfaac3954a0ed164ed9bda3c4
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-13 12:17:51 +0200

    Fix Products Title

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -12,7 +12,7 @@ const Product = ({id,title, price, img}:TProduct) => {
           alt={title}
         />
       </div>
-      <h2>{title}</h2>
+      <h2 title={title}>{title}</h2>
       <h3>{price} EGP</h3>
       <Button variant="info" style={{ color: "white" }}>
         Add to cart
