4dc8de2bab946e7b876b53bab5ef1326870a00c8
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-16 15:45:35 +0200

    Render total quantity in header basket

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,13 +1,19 @@
 import Logo from '../../../assets/svg/cart.svg?react';
 import styles from './styles.module.css';
-
+import { useAppSelector } from '@store/hooks';
 const { basketContainer, basketQuantity } = styles;
 
 function HeaderBasket() {
+  const cartItems = useAppSelector((state) => state.cart.items);
+  // console.log(Object.values(cartItems));
+  const totalQuantity = Object.values(cartItems).reduce(
+    (prev, crt) => prev + crt,
+    0
+  );
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" style={{ width: 40, height: 30 }} />
-      <div className={basketQuantity}>0</div>
+      <div className={basketQuantity}>{totalQuantity}</div>
     </div>
   );
 }
