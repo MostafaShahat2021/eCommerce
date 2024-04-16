8c170f8aa462ef160ccdaf6ee2bae5c8f8fa7f7b
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-16 14:39:44 +0200

    Create cartSlice

---
+++ src/store/cart/cartSlice.ts
@@ -0,0 +1,19 @@
+import { TProduct } from '@customTypes/product';
+import { createSlice } from '@reduxjs/toolkit';
+
+interface ICartState {
+  items: { [key: number]: number };
+  productFullInfo: TProduct[];
+}
+const initialState: ICartState = {
+  items: {},
+  productFullInfo: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
+
+export default cartSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import categories from './categories/categoriesSlice'
 import products from "./products/productsSlice"
+import cart from "./cart/cartSlice"
 const store = configureStore({
-  reducer: {categories, products},
+  reducer: {categories, products, cart},
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
