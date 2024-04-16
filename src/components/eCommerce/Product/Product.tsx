688dea20a50c8e34ba7456103aa37bd469503db7
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-16 15:13:39 +0200

    Add (addToCart) Action

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,20 +1,27 @@
-import { TProduct } from "@customTypes/product";
-import { Button } from "react-bootstrap";
-import styles from "./styles.module.css";
+import { useAppDispatch } from '@store/hooks';
+import { TProduct } from '@customTypes/product';
+import { Button } from 'react-bootstrap';
+import styles from './styles.module.css';
+import { addToCart } from '@store/cart/cartSlice';
 const { product, productImg } = styles;
 
-const Product = ({id,title, price, img}:TProduct) => {
+const Product = ({ id, title, price, img }: TProduct) => {
+  const dispatch = useAppDispatch();
+  const addToCartHandler = () => {
+    dispatch(addToCart(id));
+  };
   return (
-    <div className={product}key={id}>
+    <div className={product} key={id}>
       <div className={productImg}>
-        <img
-          src={img}
-          alt={title}
-        />
+        <img src={img} alt={title} />
       </div>
       <h2 title={title}>{title}</h2>
       <h3>{price} EGP</h3>
-      <Button variant="info" style={{ color: "white" }}>
+      <Button
+        variant="info"
+        style={{ color: 'white' }}
+        onClick={addToCartHandler}
+      >
         Add to cart
       </Button>
     </div>
