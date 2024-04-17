0ac767ab83c3010b11123aec8656c4f5e4ce7ecf
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-17 13:57:27 +0200

    Animate shopping cart total quantity with debouncing technique

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,17 +1,32 @@
-// import { getCartTotalQuantitySelectore } from '@store/cart/selectors';
 import { getCartTotalQuantitySelectore } from '@store/cart/cartSlice';
 import Logo from '../../../assets/svg/cart.svg?react';
 import styles from './styles.module.css';
 import { useAppSelector } from '@store/hooks';
-const { basketContainer, basketQuantity } = styles;
+import { useEffect, useState } from 'react';
+const { basketContainer, basketQuantity, pumpCartQuantity } = styles;
 
 function HeaderBasket() {
+  const [isAnimate, setIsAnimate] = useState(false);
   const totalQuantity = useAppSelector(getCartTotalQuantitySelectore);
-  
+  const quantityStyle = `${basketQuantity} ${
+    isAnimate ? pumpCartQuantity : ''
+  } `;
+
+  useEffect(() => {
+    if (!totalQuantity) {
+      return;
+    }
+    setIsAnimate(true);
+    const debounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [totalQuantity, setIsAnimate]);
+
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" style={{ width: 40, height: 30 }} />
-      <div className={basketQuantity}>{totalQuantity}</div>
+      <div className={quantityStyle}>{totalQuantity}</div>
     </div>
   );
 }
