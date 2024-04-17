8414a54d1f96ba8336c3f4ddba83b51bf9ba6e2b
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-17 14:32:59 +0200

    debouncing add to cart

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,14 +1,32 @@
+import { useState, useEffect } from 'react';
 import { useAppDispatch } from '@store/hooks';
 import { TProduct } from '@customTypes/product';
-import { Button } from 'react-bootstrap';
-import styles from './styles.module.css';
+import { Button, Spinner } from 'react-bootstrap';
 import { addToCart } from '@store/cart/cartSlice';
+import styles from './styles.module.css';
+
 const { product, productImg } = styles;
 
 const Product = ({ id, title, price, img }: TProduct) => {
   const dispatch = useAppDispatch();
+
+  const [isBtnClicked, setIsBtnClicked] = useState(0);
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+
+  useEffect(() => {
+    if (!isBtnClicked) {
+      return;
+    }
+    setIsBtnDisabled(true);
+    const debounce = setTimeout(() => {
+      setIsBtnDisabled(false);
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [isBtnClicked]);
+
   const addToCartHandler = () => {
     dispatch(addToCart(id));
+    setIsBtnClicked((prev) => prev + 1);
   };
   return (
     <div className={product} key={id}>
@@ -21,8 +39,16 @@ const Product = ({ id, title, price, img }: TProduct) => {
         variant="info"
         style={{ color: 'white' }}
         onClick={addToCartHandler}
+        disabled={isBtnDisabled}
       >
-        Add to cart
+        {isBtnDisabled ? (
+          <>
+            <Spinner animation="border" size="sm" />
+            loading...
+          </>
+        ) : (
+          'Add to cart'
+        )}
       </Button>
     </div>
   );
