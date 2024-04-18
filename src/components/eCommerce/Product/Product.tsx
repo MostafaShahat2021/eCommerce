df75b7e220549eb11c521118c869aea18f1e8b29
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2024-04-18 09:58:31 +0200

    Refactor btn disabled logic

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -10,23 +10,22 @@ const { product, productImg } = styles;
 const Product = ({ id, title, price, img }: TProduct) => {
   const dispatch = useAppDispatch();
 
-  const [isBtnClicked, setIsBtnClicked] = useState(0);
   const [isBtnDisabled, setIsBtnDisabled] = useState(false);
 
   useEffect(() => {
-    if (!isBtnClicked) {
+    if (!isBtnDisabled) {
       return;
     }
-    setIsBtnDisabled(true);
+    // setIsBtnDisabled(true);
     const debounce = setTimeout(() => {
       setIsBtnDisabled(false);
     }, 300);
     return () => clearTimeout(debounce);
-  }, [isBtnClicked]);
+  }, [isBtnDisabled]);
 
   const addToCartHandler = () => {
     dispatch(addToCart(id));
-    setIsBtnClicked((prev) => prev + 1);
+    setIsBtnDisabled(true);
   };
   return (
     <div className={product} key={id}>
