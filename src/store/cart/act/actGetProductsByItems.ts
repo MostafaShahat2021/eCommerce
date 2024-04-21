d8560a3b91e72ff74d285589ede5775d276774ec
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-04-21 16:12:12 +0200

    Finalize getProductsByItems

--- src/store/cart/act/actGetProductsByItems.ts
+++ src/store/cart/act/actGetProductsByItems.ts
@@ -1,11 +1,27 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "@store/index";
+import { TProduct } from "@customTypes/product";
+
+type TResponse = TProduct[]
 
 const actGetProductsByItems = createAsyncThunk("cart/actGetProductsByItems", async (_, thunkAPI) => {
-  const {reJectedWithValue, getState} = thunkAPI
+  const {rejectWithValue, getState} = thunkAPI
   const {cart} = getState() as RootState
-console.log(cart.items);
+// console.log(cart.items);
+const itemsId = Object.keys(cart.items)
+const concatenatedItemsId = itemsId.map((el) => `id=${el}`).join("&")
+// console.log(concatenatedItemsId);
+try {
+  const response = await axios.get<TResponse>(`/products?${concatenatedItemsId}`)
+  return response.data
+} catch (error) {
+  if(axios.isAxiosError(error)){
+    return rejectWithValue(error.response?.data.message || error.message)
+  } else {
+    return rejectWithValue("unexpected error")
+  }
+}
 })
 
 export default actGetProductsByItems
